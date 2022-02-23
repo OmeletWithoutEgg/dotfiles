3004aaad4090ebcc4d98844237e00556c5d2cb46
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-02-23 10:18:29 +0800

    fix install

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ FILES="vimrc zshrc gitconfig"
 # TODO: assert that vim, zsh exists
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -e NONE +'PlugInstall --sync' +qa
+vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 
 chsh -s /usr/bin/zsh
-# curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 for f in $FILES; do
 	cp $f ~/.$f
