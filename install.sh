010ce4335dda26764835512d9c38db61eb38a50d
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-02-23 10:35:37 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,19 @@
 
 FILES="vimrc zshrc gitconfig"
 
-# TODO: assert that vim, zsh exists
+if ! command -v vim zsh; then
+    echo "Remember to install vim, zsh"
+    exit
+fi
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 
 chsh -s /usr/bin/zsh
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+if [[ ! -d ~/.zplug ]]; then
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+fi
 
 for f in $FILES; do
 	cp $f ~/.$f
