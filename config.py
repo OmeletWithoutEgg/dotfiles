4bdaa4d30e275c94f7aaf87f4aed4460595c02ec
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-03-19 00:32:28 +0800

    update qutebrowser config.py

--- config.py
+++ config.py
@@ -1,26 +1,39 @@
 # ~/.config/qutebrowser/config.py
-# c.tabs.position = "top"
+
+config.unbind('ZQ')
+
+config.bind('zl', 'spawn --userscript qute-pass')
+config.bind('zul', 'spawn --userscript qute-pass --username-only')
+config.bind('zpl', 'spawn --userscript qute-pass --password-only')
+
 c.editor.command = ['konsole', '-e', 'vim', '{}'] 
 
-c.url.default_page = "https://google.com"
-c.url.start_pages = "https://codeforces.com"
-c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}"}
+c.url.default_page = 'https://google.com'
+c.url.start_pages = 'https://codeforces.com'
+c.url.searchengines = {'DEFAULT': 'https://google.com/search?q={}'}
 
-c.zoom.default = "125%"
-c.fonts.default_size = "14pt"
-c.fonts.web.family.fixed = "Hack"
+c.zoom.default = '125%'
+c.fonts.default_size = '14pt'
+c.fonts.web.family.fixed = 'Hack'
 
-from qutebrowser.api import interceptor
+c.content.blocking.method = 'both'
 
-def filter_yt(info: interceptor.Request):
-	"""Block the given request if necessary."""
-	url = info.request_url
-	if (url.host() == 'www.youtube.com' and
-			url.path() == '/get_video_info' and
-			'&adformat=' in url.query()):
-		info.block()
+c.auto_save.session = True
 
-interceptor.register(filter_yt)
+c.content.javascript.can_access_clipboard = True
 
+c.new_instance_open_target = 'tab-silent'
 
-config.load_autoconfig(True)
+# from qutebrowser.api import interceptor
+# 
+# def filter_yt(info: interceptor.Request):
+# 	"""Block the given request if necessary."""
+# 	url = info.request_url
+# 	if (url.host() == 'www.youtube.com' and
+# 			url.path() == '/get_video_info' and
+# 			'&adformat=' in url.query()):
+# 		info.block()
+# 
+# interceptor.register(filter_yt)
+
+config.load_autoconfig()
