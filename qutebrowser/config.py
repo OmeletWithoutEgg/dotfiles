4845bd7bcff2d74a8e7b18476df9d7916d10caf6
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-03-20 22:47:04 +0800

    reformat config.py

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,39 +1,33 @@
 # ~/.config/qutebrowser/config.py
+config.load_autoconfig()
 
-config.unbind('ZQ')
-
-config.bind('zl', 'spawn --userscript qute-pass')
-config.bind('zul', 'spawn --userscript qute-pass --username-only')
-config.bind('zpl', 'spawn --userscript qute-pass --password-only')
-
+c.auto_save.session = True
+c.content.blocking.method = 'both'
+c.content.javascript.can_access_clipboard = True
 c.editor.command = ['konsole', '-e', 'vim', '{}'] 
-
+c.fonts.default_size = '14pt'
+c.fonts.web.family.fixed = 'Hack'
+c.new_instance_open_target = 'tab-silent'
 c.url.default_page = 'https://google.com'
 c.url.start_pages = 'https://codeforces.com'
 c.url.searchengines = {'DEFAULT': 'https://google.com/search?q={}'}
-
 c.zoom.default = '125%'
-c.fonts.default_size = '14pt'
-c.fonts.web.family.fixed = 'Hack'
 
-c.content.blocking.method = 'both'
+config.unbind('ZQ')
+config.bind(';v', 'hint links spawn mpv {hint-url}')
+config.bind('zl', 'spawn --userscript qute-pass')
+config.bind('zpl', 'spawn --userscript qute-pass --password-only')
+config.bind('zul', 'spawn --userscript qute-pass --username-only')
 
-c.auto_save.session = True
+## YT blocking
+from qutebrowser.api import interceptor
 
-c.content.javascript.can_access_clipboard = True
+def filter_yt(info: interceptor.Request):
+	"""Block the given request if necessary."""
+	url = info.request_url
+	if (url.host() == 'www.youtube.com' and
+			url.path() == '/get_video_info' and
+			'&adformat=' in url.query()):
+		info.block()
 
-c.new_instance_open_target = 'tab-silent'
-
-# from qutebrowser.api import interceptor
-# 
-# def filter_yt(info: interceptor.Request):
-# 	"""Block the given request if necessary."""
-# 	url = info.request_url
-# 	if (url.host() == 'www.youtube.com' and
-# 			url.path() == '/get_video_info' and
-# 			'&adformat=' in url.query()):
-# 		info.block()
-# 
-# interceptor.register(filter_yt)
-
-config.load_autoconfig()
+interceptor.register(filter_yt)
