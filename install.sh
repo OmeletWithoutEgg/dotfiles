7f695384f65376278f5aa6252e7a230aee1247bc
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-03-31 01:29:12 +0800

    update scripts

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 
 chsh -s /usr/bin/zsh
-if [[ ! -d ~/.zi ]]; then
+if [[ ! -d "$HOME/.zi" ]]; then
     mkdir -p "$HOME/.zi" && chmod g-rwX "$HOME/.zi"
     git clone -q --depth=1 \
         --branch "main" https://github.com/z-shell/zi "$HOME/.zi/bin"
@@ -30,7 +30,7 @@ for f in $FILES; do
     cp $f ~/.$f
 done
 
+# cp ./qutebrowser/* ~/.config/qutebrowser/
 # cat ibus-env >> /etc/environment
-# cp .config.py ~/.config/qutebrowser/
 # cp ibus-daemon.desktop ~/.config/autostart/
 # chmod +x ~/.config/autostart/ibus-daemon.desktop
--- update.sh
+++ update.sh
@@ -6,5 +6,4 @@ for f in $FILES; do
 	cp ~/.$f $f
 done
 
-# cat ibus-env >> /etc/environment
 cp ~/.config/qutebrowser/config.py qutebrowser/config.py
