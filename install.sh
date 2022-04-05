9ff4120c99a1a960056c1094074209fff15bdcba
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-04-05 23:53:01 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 
+echo "chsh /usr/bin/zsh"
 chsh -s /usr/bin/zsh
 if [[ ! -d "$HOME/.zi" ]]; then
     mkdir -p "$HOME/.zi" && chmod g-rwX "$HOME/.zi"
@@ -30,7 +31,9 @@ for f in $FILES; do
     cp $f ~/.$f
 done
 
-# cp ./qutebrowser/* ~/.config/qutebrowser/
+if [[ -d ~/.config/qutebrowser/ ]]; then
+    cp ./qutebrowser/* ~/.config/qutebrowser/
+fi
 # cat ibus-env >> /etc/environment
 # cp ibus-daemon.desktop ~/.config/autostart/
 # chmod +x ~/.config/autostart/ibus-daemon.desktop
