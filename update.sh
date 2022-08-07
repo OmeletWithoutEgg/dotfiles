3a0fd2829042e08a3d935217547e9f3f4abed064
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-08-07 23:28:32 +0800

    make update.sh cleaner

--- update.sh
+++ update.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+DIRS=(
+    config/nvim/
+    config/doom/
+)
+
 FILES=(
     vimrc zshrc zshenv
     p10k.zsh
     tmux.conf
     config/git/config
+    config/fontconfig/fonts.conf
     config/qutebrowser/config.py
     vim/UltiSnips/tex.snippets
-
-    config/nvim/init.lua
-    config/nvim/lua/lsp.lua
-    config/nvim/lua/plugins.lua
-
-    config/doom/config.el
-    config/doom/custom.el
-    config/doom/init.el
-    config/doom/lisp/markdown-agda-mode.el
-    config/doom/packages.el
 )
 
 if [[ -n `git status -s -uall` ]]; then
@@ -28,4 +24,9 @@ fi
 for f in ${FILES[@]}; do
     echo "cp ~/.$f $f"
     cp --preserve=mode ~/.$f $f
+done
+
+for d in ${DIRS[@]}; do
+    echo "rsync -r -v ~/.$d $d"
+    rsync -r ~/.$d $d
 done
