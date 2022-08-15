ca604a724e8d626e4a41389202852f91531d5e78
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-08-15 15:03:54 +0800

    turn off warn about missing glyphs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,8 +9,10 @@ end)
 
 return {
   color_scheme = 'Breeze',
-  check_for_updates = false,
+  -- check_for_updates = false,
+  warn_about_missing_glyphs = false,
   -- color_scheme = 'Gruvbox Dark',
-  font = wezterm.font_with_fallback({ 'Hack', 'Noto Sans CJK TC' }),
+  font = wezterm.font_with_fallback({ 'Hack Nerd Font', 'Noto Sans CJK TC' }),
+  font_size = 14,
   window_decorations = 'NONE',
 }
