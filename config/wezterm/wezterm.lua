661879233a32b36ff4661639ce23562c87d5b28c
OmeletWithoutEgg <2qbingxuan@gmail.com>
2022-08-16 14:29:57 +0800

    make background a little darker

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,16 +3,21 @@ local wezterm = require('wezterm')
 local mux = wezterm.mux
 
 wezterm.on('gui-startup', function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():maximize()
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
 end)
 
+local breeze_dark = wezterm.get_builtin_color_schemes()['Breeze']
+breeze_dark.background = '#232627'
+
 return {
-  color_scheme = 'Breeze',
-  -- check_for_updates = false,
-  warn_about_missing_glyphs = false,
-  -- color_scheme = 'Gruvbox Dark',
-  font = wezterm.font_with_fallback({ 'Hack Nerd Font', 'Noto Sans CJK TC' }),
-  font_size = 14,
-  window_decorations = 'NONE',
+    color_scheme = 'BreezeDark',
+    color_schemes = { ['BreezeDark'] = breeze_dark },
+    -- check_for_updates = false,
+    warn_about_missing_glyphs = false,
+    -- color_scheme = 'Gruvbox Dark',
+    font = wezterm.font_with_fallback({ 'Hack Nerd Font', 'Noto Sans CJK TC' }),
+    font_size = 14,
+    window_decorations = 'NONE',
+    -- enable_kitty_graphics = true,
 }
