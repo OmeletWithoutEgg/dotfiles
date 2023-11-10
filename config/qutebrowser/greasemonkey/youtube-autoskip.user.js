5c37207a3a2f0c40d8847ae4b5c0f3a36c2f1bbe
OmeletWithoutEgg <2qbingxuan@gmail.com>
2023-11-11 01:50:42 +0800

    Update greasemonkey script in qutebrowser

--- config/qutebrowser/greasemonkey/youtube-autoskip.user.js
+++ config/qutebrowser/greasemonkey/youtube-autoskip.user.js
@@ -3,25 +3,46 @@
 // @version      1.0.2
 // @description  Speed up and skip YouTube ads automatically 
 // @author       codiac-killer
-// @match        *://*.youtube.com/*
-// @exclude      *://*.youtube.com/subscribe_embed?*
+// @match        *://*.youtube.com/watch*
 // ==/UserScript==
-
-let main = new MutationObserver(() => {
+const removeAds = () => {
   // Get skip button and click it
-  let btn = document.getElementsByClassName('ytp-ad-skip-button ytp-button').item(0)
-  if (btn) {
-    btn.click()
+  for (let btn of [...document.getElementsByClassName("ytp-ad-skip-button ytp-button")]) {
+    btn.click();
+    console.log("classical button clicked");
+  }
+  for (let btn of [...document.getElementsByClassName("ytp-ad-skip-button-modern ytp-button")]) {
+    btn.click();
+    console.log("modern button clicked");
   }
 
   // (unskipable ads) If skip button didn't exist / was not clicked speed up video
-  const ad = [...document.querySelectorAll('.ad-showing')][0];
+  const ad = [...document.querySelectorAll(".ad-showing")][0];
   if (ad) {
+    console.log("ad found");
     // Speed up and mute
-    document.querySelector('video').playbackRate = 15;
-    document.querySelector('video').muted = true;
+    document.querySelector("video").playbackRate = 16;
+    document.querySelector("video").muted = true;
+  } else {
+    console.log("ad not found");
+  }
+};
+
+const main = new MutationObserver(() => {
+  // call if ad watcher called late
+  // removeAds();
+  const adContainer = document.getElementsByClassName("video-ads ytp-ad-module").item(0);
+  if (adContainer) {
+    main.disconnect();
+    console.log("adContainer found, observing...");
+    console.log(adContainer);
+    new MutationObserver(removeAds).observe(adContainer, {attributes: true, characterData: true, childList: true});
+  } else {
+    console.log("adContainer not found!");
   }
 })
 
-main.observe(document.getElementsByClassName('video-ads ytp-ad-module').item(0),
-  { attributes: true, characterData: true, childList: true })
+// call in case page loads this script late and no mutations to observe
+removeAds();
+// look for ad container
+main.observe(document.body, {attributes: false, characterData: false, childList: true, subtree: true});
