09d170f68e676cdf3bcb6aa303fc9da52cb726c3
OmeletWithoutEgg <2qbingxuan@gmail.com>
2026-06-13 14:23:46 +0800

    Add treesitter back

---
+++ config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  'romus204/tree-sitter-manager.nvim',
+  opts = {
+    ensure_installed = {
+      'lua', 'vim', 'vimdoc',
+      'dockerfile', 'make',
+      'c', 'cpp', 'rust', 'javascript',
+      'python', 'ruby', 'bash',
+      'latex', 'haskell',
+      'toml', 'yaml', 'html', 'css', 'rasi', 'scss',
+      'markdown', 'markdown_inline',
+    }
+  },
+}
