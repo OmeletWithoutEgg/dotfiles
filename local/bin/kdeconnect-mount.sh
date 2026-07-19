e02f177df760a51b5f4fdaf8e2b4d5f01a2c0098
OmeletWithoutEgg <2qbingxuan@gmail.com>
2026-07-19 18:48:34 +0800

    Update local bin scripts

--- local/bin/kdeconnect-mount.sh
+++ local/bin/kdeconnect-mount.sh
@@ -4,31 +4,34 @@ set -e
 
 function echoerr() { echo "$@" 1>&2; }
 
-echoerr "kdeconnect-cli listing devices..."
-
 function choose() {
   if [[ "wayland" == "$XDG_SESSION_TYPE" ]]; then
-    wofi --dmenu "$@"
+    GTK_IM_MODULE="" wofi --dmenu "$@"
   else
     rofi -dmenu -sync "$@"
   fi
 }
 
 function query_dbus() {
-  qdbus org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONE_ID"/sftp "$@"
+  qdbus6 org.kde.kdeconnect /modules/kdeconnect/devices/"$PHONE_ID"/sftp "$@"
 }
 
-PHONE_ID=$(kdeconnect-cli --list-devices --id-only | choose -p phone_id)
-echoerr "phone_id=$PHONE_ID"
+function main() {
+  echoerr "kdeconnect-cli listing devices..."
+  PHONE_ID=$(choose -p phone_id <<<$(kdeconnect-cli --list-devices --id-name-only) | cut -f 1 -d ' ')
+  echoerr "phone_id=$PHONE_ID"
 
-MOUNTED=$(query_dbus isMounted)
-echoerr "MOUNTED=$MOUNTED"
+  MOUNTED=$(query_dbus isMounted)
+  echoerr "MOUNTED=$MOUNTED"
 
-if [[ "$MOUNTED" == "false" ]]; then
-  echoerr "mounting..."
-  query_dbus mountAndWait # >/dev/null
-  echoerr "mount done!"
-fi
+  if [[ "$MOUNTED" == "false" ]]; then
+    echoerr "mounting..."
+    query_dbus mountAndWait # >/dev/null
+    echoerr "mount done!"
+  fi
 
-MOUNT_POINT=$(query_dbus getDirectories | choose -p mount_point | cut -f 1 -d ':')
-echo $MOUNT_POINT
+  MOUNT_POINT=$(query_dbus getDirectories | choose -p mount_point | cut -f 1 -d ':')
+  echo "$MOUNT_POINT"
+}
+
+main
